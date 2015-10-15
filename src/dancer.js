2dd05476d2e48ef1bc9526857f92dcfa1a76fcad
acrobat130 <shanna.sullivan@gmail.com>
2015-10-14 19:35:19 -0700

    Completed Pseudoclassical Dancer and passed tests

--- src/dancer.js
+++ src/dancer.js
@@ -1,36 +1,30 @@
-// Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
+// Creates and returns a new Dancer object that can step
 
-  var dancer = {};
-
+var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
+  this.step();
+  this.setPosition(top, left);
+}
 
-
-  dancer.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left){
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
+Dancer.prototype.step = function(){
+// the basic Dancer doesn't do anything interesting at all on each step,
+// it just schedules the next step
+  setTimeout(this.step, this.timeBetweenSteps);
 };
 
-var happyDancer = makeDancer(500, 500, 1000);
+Dancer.prototype.setPosition = function(top, left) {
+// Use css top and left properties to position where it belongs on the page. See http://api.jquery.com/css/
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  console.log('this.$node is: ', this.$node);
+  this.$node.css(styleSettings);
+};
+
+var happyDancer = new Dancer(500, 500, 1000);
 console.log('happyDancer is : ', happyDancer);
+
+
+
