a8993e95035462c660f7b5d857215b970b86242c
acrobat130 <shanna.sullivan@gmail.com>
2015-10-15 19:31:24 -0700

    added psychedelic dancers!

--- src/psychedelicDancer.js
+++ src/psychedelicDancer.js
@@ -1,12 +1,36 @@
-var BlinkyDancer = function(top, left, timeBetweenSteps){
+var PsychedelicDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.toggleCount = 0;
 };
 
-BlinkyDancer.prototype = Object.create(Dancer.prototype);
-BlinkyDancer.prototype.constructor = BlinkyDancer;
+PsychedelicDancer.prototype = Object.create(Dancer.prototype);
+PsychedelicDancer.prototype.constructor = PsychedelicDancer;
 
-BlinkyDancer.prototype.step = function(){
+PsychedelicDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
+  this.toggleCount++;
   this.$node.toggle();
+  console.log("count before if",this.toggleCount);
+  // if (this.toggleCount % 2 === 0) { // change every 2 toggles
+  //   var styleSettings = {
+  //     border: "25px solid purple",
+  //     "border-radius": "25px"
+  //   };
+  //   this.$node.css(styleSettings);  //consider else?
+  // };
+  if (this.toggleCount % 5 === 0) { //|| 75 < this.toggleCount < 500) {
+    var dancerColorR = Math.floor(Math.random()*255);
+    var dancerColorG = Math.floor(Math.random()*255);
+    var dancerColorB = Math.floor(Math.random()*255);
+    var randomColor = "rgb(" + dancerColorR + ", " + dancerColorG + ", " + dancerColorB + ")";
+    var styleSettings = {
+      // border: "10px",
+      "border-radius": "10px",
+      "border-color" : randomColor
+    };
+    this.$node.css(styleSettings);  //consider else?
+  }
 };
+
+
 
